03f5dfeaa0903e1709f1de8c112833a5c5159969
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-05-14 11:00:24 -0300

    adding death to enemies

--- components/health_bar.gd
+++ components/health_bar.gd
@@ -2,9 +2,13 @@ extends Node
 
 class_name HealthBar
 
+signal die
+
 @export var max_health: float = 10.0
+@export var death_effect: PackedScene
 
 var health: float = max_health
 
 func damage(amount: float) -> void:
 	health = clampf(health - amount, 0, max_health)
+	if(health == 0.0): die.emit(death_effect)
--- objects/enemies/enemy.gd
+++ objects/enemies/enemy.gd
@@ -5,6 +5,17 @@ class_name Enemy
 # WARNING: HealthBar should be searched by type, not by name
 @onready var health_bar: HealthBar = $HealthBar
 
+func _ready():
+	health_bar.die.connect(on_death)
+
 func damage(amount: float) -> void:
 	if health_bar: health_bar.damage(amount)
 	print(self.name, " damaged, health: ", health_bar.health)
+
+func on_death(death_effect_scene: PackedScene) -> void:
+	if death_effect_scene:
+		var death_effect = death_effect_scene.instantiate()
+		death_effect.position = position
+		get_parent().add_child(death_effect)
+	
+	queue_free()
