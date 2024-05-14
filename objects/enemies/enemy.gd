20a7d9886ae01a70628962bd5f7e5314f73dfd31
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-05-14 11:18:22 -0300

    adding damage effect

--- objects/enemies/enemy.gd
+++ objects/enemies/enemy.gd
@@ -6,11 +6,19 @@ class_name Enemy
 @onready var health_bar: HealthBar = $HealthBar
 
 func _ready():
+	health_bar.damaged.connect(on_damaged)
 	health_bar.die.connect(on_death)
 
 func damage(amount: float) -> void:
 	if health_bar: health_bar.damage(amount)
 	print(self.name, " damaged, health: ", health_bar.health)
+
+func on_damaged(damage_color: Color) -> void:
+	modulate = damage_color
+	var tween: Tween = create_tween()
+	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_QUINT)
+	tween.tween_property(self, "modulate", Color.WHITE, .25)
 
 func on_death(death_effect_scene: PackedScene) -> void:
 	if death_effect_scene:
