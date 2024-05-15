a9264847d68f05ec61eb490cc7e0f9d985d79931
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-05-15 16:23:39 -0300

    abstracting damage indicator to health update indicator

--- components/ui/health_update_indicator/ui_health_update_indicator.gd
+++ components/ui/health_update_indicator/ui_health_update_indicator.gd
@@ -3,8 +3,10 @@ extends Control
 ## Expects a HealthBar component from which this UIDamageIndicator will get its
 ## values
 @export var health_bar: HealthBar
+@export var show_damage: bool = true
+@export var show_heal: bool = true
 
-@onready var label_scene: PackedScene = preload("res://components/ui/damage_indicator/ui_damage_indicator_label.tscn")
+@onready var label_scene: PackedScene = preload("res://components/ui/health_update_indicator/ui_health_update_indicator_label.tscn")
 
 func _ready() -> void:
 	if !health_bar: return
@@ -13,12 +15,16 @@ func _ready() -> void:
 	health_bar.healed.connect(on_healed)
 
 func on_damaged(amount: float) -> void:
+	if !show_damage: return
+	
 	var label: Label = label_scene.instantiate()
 	label.text = str(amount)
 	label.label_settings.font_color = Color.RED
 	add_child(label)
 
 func on_healed(amount: float) -> void:
+	if !show_heal: return
+	
 	var label: Label = label_scene.instantiate()
 	label.text = str(amount)
 	label.label_settings.font_color = Color.DEEP_SKY_BLUE
