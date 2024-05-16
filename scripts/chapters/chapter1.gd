73e0d9f7d21a86c6433c0ca5d329631f7a4d8ca8
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-05-16 18:05:48 -0300

    testing the chapter UI integration

--- scripts/chapters/chapter1.gd
+++ scripts/chapters/chapter1.gd
@@ -1,13 +1,19 @@
 extends State
 
-@onready var enemy_spawner = $"../../EnemySpawner"
+@onready var enemy_spawner: SceneSpawner = $"../../EnemySpawner"
+@onready var ui: CanvasLayer = $"../../UI"
 const GOBLIN = preload("res://objects/enemies/goblin/goblin.tscn")
+const UI_CHAPTER_TITLE = preload("res://components/ui/chapter_title/ui_chapter_title.tscn")
 
 func _ready() -> void:
 	state_name = "First wave"
 
 func _enter_state() -> void:
-	print(state_name + " started!")
+	var ui_chapter_title: Node = UI_CHAPTER_TITLE.instantiate()
+	ui_chapter_title.title = state_name
+	ui.add_child(ui_chapter_title)
+	ui_chapter_title.disappeared.connect(on_title_disappear)
+
+func on_title_disappear() -> void:
 	enemy_spawner.packed_scenes.append(GOBLIN)
 	enemy_spawner.spawn_per_minute = 60
-	pass
