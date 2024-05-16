cae01e5efe1690bcd2c61778a71ad091fe4a7cb9
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-05-16 16:53:06 -0300

    testing StateManagers being used as game chapters

--- scripts/chapters/chapter1.gd
+++ scripts/chapters/chapter1.gd
@@ -1,8 +1,13 @@
 extends State
 
+@onready var enemy_spawner = $"../../EnemySpawner"
+const GOBLIN = preload("res://objects/enemies/goblin/goblin.tscn")
+
 func _ready() -> void:
 	state_name = "First wave"
 
 func _enter_state() -> void:
-	get_tree()
+	print(state_name + " started!")
+	enemy_spawner.packed_scenes.append(GOBLIN)
+	enemy_spawner.spawn_per_minute = 60
 	pass
