6ea32ad3e715169667c40e82ae2a44f54bf5f94b
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-05-17 12:06:36 -0300

    implementing a fire object

--- components/attack_area.gd
+++ components/attack_area.gd
@@ -3,19 +3,19 @@ extends Area2D
 ## Area2D that serves as a hitbox for an attack
 class_name AttackArea
 
-@export var damage: float
-@export_range(-1, 1) var attack_range: float
-@export var target_group: String
+@export var damage: float = 0
+@export_range(-1, 1) var attack_range: float = -1
+@export var target_group: String = ""
 
 func attack(
-	attack_direction: Vector2,
+	attack_direction: Vector2 = Vector2.RIGHT,
 	damage: float = self.damage,
 	attack_range: float = self.attack_range,
 	target_group: String = self.target_group
 ) -> void:
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
-		if !(body.is_in_group(target_group)): continue
+		if target_group != "" && !(body.is_in_group(target_group)): continue
 		if !(body is Character): continue
 		
 		var enemy: Character = body
---
+++ scripts/fire.gd
@@ -0,0 +1,4 @@
+extends AttackArea
+
+func burn() -> void:
+	attack()
