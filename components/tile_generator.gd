3e4e537dd289f46713ae5f05202f3f358985924a
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-05-17 11:37:36 -0300

    implementing a primary tile generator

--- components/tile_generator.gd
+++ components/tile_generator.gd
@@ -1,19 +1,26 @@
 extends TileMap
 
-@onready var camera: Camera2D = $"../Camera2D"
+## Provides methods to generate tiles dinamically
+class_name TileGenerator
+
+## The camera that will notify this generator when to generate
+@export var camera: CameraTracker
+
+var tile_size: Vector2 = tile_set.tile_size
 
 func _ready() -> void:
-	print("Ready")
-	pass
+	camera.updated.connect(on_camera_moved)
 
-func _process(delta: float) -> void:
-	var canvas_transform = get_canvas_transform()
-	var canvas_origin = canvas_transform.get_origin()
-	var canvas_scale = canvas_transform.get_scale()
-	var top_left = -canvas_origin / canvas_scale
-	var view_size = get_viewport_rect().size / canvas_scale
-	var bottom_right = top_left + view_size
-	#print(bottom_right)
-	#print(top_left)
-	#print(get_viewport().get_final_transform())
-	#print(camera.get_viewport_transform())
+func generate_tiles(from: Vector2, to: Vector2) -> void:
+	for x in range(from.x, to.x, tile_size.x):
+		for y in range(from.y, to.y, tile_size.y):
+			var map_position: Vector2i = local_to_map(Vector2(x, y))
+			var current_cell: int = get_cell_source_id(0, map_position)
+			
+			if current_cell != -1: continue
+			
+			# 0 represents the ground layer
+			set_cell(0, map_position, 0, Vector2i(1, 1))
+
+func on_camera_moved(top_left: Vector2, bottom_right: Vector2) -> void:
+	generate_tiles(top_left - tile_size, bottom_right + tile_size)
